b850d3e8e4425997fccff90eca9620d1b4d97a2d
Abhishek51116 <abhishekaggarwal00@gmail.com>
2019-09-11 18:34:18 +0530

    App decoration and Skeleton

---
+++ index.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Notes APP</title>
+     <link href="styling.css" rel="stylesheet" type="text/css">
+       <link href="https://fonts.googleapis.com/css?family=Chilanka&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <!-- Navigation BAR -->
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Online Notes</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Help</a>
+      </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Contact Us</a>
+      </li>
+    </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#contact">Login</a></li>
+        </ul>
+  </div>
+</nav>     
+      <!-- Jumbotron with sign up Button -->
+      <div class="jumbotron" id="myContainer">
+  <h1 class="display-4">Online NOtes App</h1>
+  <p class="lead">Easy to Use protect all your notes!</p>
+  <hr class="my-4">
+  
+  <a class="btn  btn-lg green button" href="#" role="button">Sign Up-It's Free</a>
+</div>
+      
+      <!-- Login Form -->
+      
+      
+      <!-- sign up form -->
+      
+      <!-- Forgot password form-->
+      
+      <!-- Footer -->
+      <div class="footer">
+      <div class="container">
+          <p><b>
+          &copy; Abhishek Aggarwal 2018- <?php 
+              $year = date("Y-m-d");
+              echo $year;
+              ?>
+              </b>
+          </p>
+          </div>
+      </div>
+          
+      
+
+      
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
